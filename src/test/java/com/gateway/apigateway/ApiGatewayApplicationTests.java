3f43f7c287e7020df92878d76e9cb16b66035061
Mahadri <raj.rajeev2244@gmail.com>
2024-03-07 15:20:16 +0530

    First Commit

--- src/test/java/com/gateway/apigateway/ApiGatewayApplicationTests.java
+++ src/test/java/com/gateway/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.gateway.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ApiGatewayApplicationTests {
 
 	@Test
